06d81e9b3e3f9c38313067b8265e964d58839675
whoamizx <544722646@qq.com>
2024-04-11 11:41:48 +0800

    change 状压dp2 note

--- dp/状压dp2.cpp
+++ dp/状压dp2.cpp
@@ -1,40 +1,41 @@
+// 找图上最长的链
+// https://codeforces.com/contest/1950/problem/G
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-// 找图上最长的链
-// https://codeforces.com/contest/1950/problem/G
-string a[20];
-string b[20];
-vector<int> edges[20];
-// 第一维是包含的点,第二维是最后加入的点
-bool dp[1 << 20][20];
-
+const int N = 20;
+string a[N];
+string b[N];
+vector<int> edges[N];
+bool dp[1 << 16][N];
+int n;
 void dfs(int state, int now)
 {
     dp[state][now] = true;
     for (auto y : edges[now])
     {
-        // y已经包含在之前的里面了,不能再加入,并且这个状态之前没有出现过
-        if (state & (1 << y) || dp[state | (1 << y)][y])
+        // 下一个位置未在路线中,且没被访问过
+        if (((state >> y) & 1) == 0 && dp[state + (1 << y)][y] == 0)
         {
-            continue;
-        }
-        else
-        {
-            dfs(state | (1 << y), y);
+            dfs(state + (1 << y), y);
         }
     }
 }
-
 signed main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int n;
         scanf("%lld", &n);
-        // dp数组初始化
+        // 初始化
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+            cin >> b[i];
+
+            edges[i - 1].clear();
+        }
         for (int i = 0; i < (1 << n); i++)
         {
             for (int j = 0; j < n; j++)
@@ -42,37 +43,25 @@ signed main()
                 dp[i][j] = false;
             }
         }
-        // 初始化
-        for (int i = 0; i < n; i++)
+        // 建图
+        for (int i = 1; i <= n; i++)
         {
-            edges[i].clear();
-        }
-
-        /// 建图
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-            cin >> b[i];
-        }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 1; j <= n; j++)
             {
                 if (a[i] == a[j] || b[i] == b[j])
                 {
-                    edges[i].push_back(j);
-                    edges[j].push_back(i);
+                    edges[i - 1].push_back(j - 1);
+                    edges[j - 1].push_back(i - 1);
                 }
             }
         }
-        // 遍历顺序,从每个点开始找最长链
+        // 正向dp
         for (int i = 0; i < n; i++)
         {
-            // 当前状态,只包含第一个点,并且最后的点就是这个点
             dp[1 << i][i] = true;
             dfs(1 << i, i);
         }
-
+        // 统计答案
         int ans = 0;
         for (int i = 0; i < (1 << n); i++)
         {
@@ -80,12 +69,10 @@ signed main()
             {
                 if (dp[i][j])
                 {
-                    // 最长链就是1最多并且dp是true(合法)的
                     ans = max(ans, __builtin_popcount(i) * 1LL);
                 }
             }
         }
-
         printf("%lld\n", n - ans);
     }
     return 0;
